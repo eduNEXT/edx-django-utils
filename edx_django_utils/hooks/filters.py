48e475ac79b578bc5c03424d055d42d378e9ed9c
mariagrimaldi <maria.grimaldi@edunext.co>
2021-03-10 14:12:37 -0400

    Changed filter behavior to accept just the result from previous pipeline step

--- edx_django_utils/hooks/filters.py
+++ edx_django_utils/hooks/filters.py
@@ -16,18 +16,22 @@ def do_filter(trigger_name, *args, **kwargs):
     Will check in the django setting HOOKS_EXTENSIONS the trigger_name key and call their configured
     functions.
 
+    Each filter function must a dictionary. This returned value will be fed to the next filter function
+    if exists, if not, this value will be returned by do_filter.
+
     Params:
         trigger_name: a string that determines which is the trigger of this filter.
     """
-
     filter_functions, are_async = get_cached_functions_for_hook(trigger_name)
+
     out = kwargs.copy()
+
     for filter_function in filter_functions:
         try:
-            result = filter_function(*args, **out) or {}
-            if not isinstance(result, dict):
-                return result
-            out.update(result)
+            out = filter_function(*args, **out) or {}
+
+            args = () if args else args  # Reset to empty args.
+
         except Exception as exc:  # pylint: disable=broad-except
             # We're catching this because we don't want the core to blow up when a
             # hook is broken. This exception will probably need some sort of
