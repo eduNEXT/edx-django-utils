01557180fe962c49495add93a93a114dd7f541c6
mariagrimaldi <maria.grimaldi@edunext.co>
2021-03-19 16:37:31 -0400

    fix: Add __str__ method to HookException

--- edx_django_utils/hooks/exceptions.py
+++ edx_django_utils/hooks/exceptions.py
@@ -9,8 +9,11 @@ class HookException(Exception):
     the actions/filters that is executing raises it.
     """
 
-    def __init__(self, message='', redirect_to=None, status_code=None):
+    def __init__(self, message="", redirect_to=None, status_code=None):
         super().__init__()
         self.message = message
         self.redirect_to = redirect_to
         self.status_code = status_code
+
+    def __str__(self):
+        return "HookException: {}".format(self.message)
